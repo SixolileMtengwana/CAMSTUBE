99712943a60e110f737086b00551a900a29da773
Sixolile Mtengwana <mtengwanas@gmail.com>
2017-10-30 12:26:42 +0200

    Updated Initial Application
    * Installations
       - Bootstrap
       - Font-Awesome

--- camstube/src/app/app.module.ts
+++ camstube/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home/home.component';
+
+const routes: Routes=[
+  { path:'', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot( routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
